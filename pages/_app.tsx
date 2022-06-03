389ae554ace972fb94c82c6ea5036314b4afb57b
kokily <kokily@naver.com>
2022-06-04 06:16:18 +0900

    Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,28 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { UserContextProvider } from '../libs/context/UserContext';
+import * as ga from '../libs/utils/ga';
 
 function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(new QueryClient());
+  const router = useRouter();
+
+  useEffect(() => {
+    function handleRouteChange(url: URL) {
+      ga.pageView(url);
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
@@ -14,6 +30,24 @@ function App({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <title>D&K Dreams Blog - Welcome</title>
       </Head>
+      <Script
+        async
+        id="google_analytics"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`}
+      />
+      <Script
+        id="google_tag"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+          `,
+        }}
+      />
       <UserContextProvider>
         <QueryClientProvider client={queryClient}>
           <Hydrate state={pageProps.dehydratedState}>
